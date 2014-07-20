0f421e417754f33b3d31d8c1a7911d8ef2e2f307
Radhakrishna-N <rksending@gmail.com>
2014-07-20 12:50:33 +0530

    Intial set-up of app with status migration

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TreebookNew::Application.config.secret_token = 'ea9c1efcf02e539e92e0c44668652bf2234b0a61df0c99880cb9e0cdedd53a5a56adea6165b4cc053c1fae056b0905f5f9eca6e9dedd36053192f2f88e32caa8'
